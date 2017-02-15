4cd0f66f6de61d4e243494427295edd0e6237eb8
Alan Kantserov <kantserov@MacBook-Pro-Alan.local>
2017-02-15 01:55:29 -0500

    Initial Commit

---
+++ QuizCS/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  QuizCS
+//
+//  Created by Alan Kantserov on 15.02.17.
+//  Copyright © 2017 Alan Kantserov. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
